c953c27a0a9052c59185f615c5da09065f33370b
Peyman Azad <peyman.azad@outlook.com>
2016-09-16 17:18:24 +0430

    initializing platformArtist

--- js/artists.js
+++ js/artists.js
@@ -43,3 +43,65 @@ groundArtist.prototype = {
                                           cell.width, cell.height);
     }
 }
+
+spriteArtist = function(sprite, context, spritesheet, cells, state){
+    this.spritesheet = spritesheet;
+    this.context = context;
+    this.sprite = sprite;
+    this.cells = cells;
+    this.state = state;
+    this.cellIndex = 0;
+}
+spriteArtist.prototype = {
+    draw: function(){
+        var cell = this.cells[this.state][this.cellIndex];
+
+        this.context.drawImage(this.spritesheet, cell.left, cell.top,
+                                            cell.width, cell.height,
+                                            this.sprite.left, this.sprite.top,
+                                            cell.width, cell.height);
+    },
+    advance: function(){
+        if (this.cellIndex === this.cells[this.state].length-1) {
+            this.cellIndex = 0;
+        }
+        else {
+            this.cellIndex++;
+        }
+    }
+}
+
+platformArtist = function(sprite, context, spritesheet, cell, shape){
+    this.sprite = sprite;
+    this.context = context;
+    this.spritesheet = spritesheet;
+    this.cell = cell;
+    this.shape = shape;
+}
+platformArtist.prototype = {
+    draw: function(){
+        switch (this.shape) {
+            case "Rectangle":
+                this.drawRectangle();
+                break;
+        
+            default:
+                this.drawRectangle();
+                break;
+        }
+    },
+    drawRectangle: function(){
+        for(var top = this.sprite.top; top < this.sprite.top + this.sprite.height; top += this.cell.height)
+            drawRow(top, this.sprite.left, this.sprite.left + this.sprite.width);
+    },
+    drawRow: function(top, leftRow, rightRow){
+        for(var left = leftRow; left < rightRow; left += this.cell.width)
+            doawCell(left, top);
+    },
+    drawCell: function(left, top){
+        this.context.drawImage(this.spritesheet, cell.left, cell.top,
+                                            cell.width, cell.height,
+                                            left, top,
+                                            cell.width, cell.height);
+    }
+}
